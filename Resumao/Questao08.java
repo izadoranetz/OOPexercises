fd480bc03ebc17bdeba7ebb9336706f08d73cb75
izadoranetz <izadora.ns@gmail.com>
2023-04-16 20:45:16 -0300

    exercícios 6 a 8

---
+++ Resumao/Questao08.java
@@ -0,0 +1,15 @@
+package Resumao;
+
+/* 8) Faça um algoritmo que leia três notas de um aluno, calcule e escreva a média final deste aluno. 
+Considerar que a média é ponderada e que o peso das notas é 2, 3 e 5. Fórmula para o cálculo da média final é:
+
+                 n1 * 2 + n2 * 3 + n3 * 5 
+mediafinal = ---------------------------------
+                             10
+
+
+ */
+
+public class Questao08 {
+    
+}
