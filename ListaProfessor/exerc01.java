a31fb88a57091fa0bf9a67f21f477949fea73220
izadoranetz <izadora.ns@gmail.com>
2023-04-17 00:18:00 -0300

    organizando arquivos

---
+++ ListaProfessor/exerc01.java
@@ -0,0 +1,14 @@
+package ListaProfessor;
+import java.util.Scanner;
+
+public class exerc01
+{
+	public static void main(String[] args) {
+		System.out.println("Digite o valor: ");
+		Scanner scan = new Scanner(System.in);
+		int valor = scan.nextInt();
+    
+		System.out.println("O dobro de " + valor + " é " + valor * 2 + ".");
+		
+	}
+}
---
+++ ListaProfessor/exerc04.java
@@ -0,0 +1,21 @@
+package ListaProfessor;
+/*4) Ler um valor e escrever maior, igual ou menor a zero.*/
+
+import java.util.Scanner;
+
+public class exerc04
+{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Insira um número: ");
+		int numero = scan.nextInt();
+		
+		if(numero < 0 ){
+		    System.out.println("Número menor que zero");
+		} else if (numero > 0) {
+		    System.out.println("Número maior que zero");
+		} else {
+		    System.out.println("Número igual a zero");
+		}
+	}
+}
