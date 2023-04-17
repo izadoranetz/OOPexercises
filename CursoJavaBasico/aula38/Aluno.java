035e2fcb78b91f1402d4c446ed2c7c8f306b0e93
Izadora Netz <izadora.ns@gmail.com>
2023-04-17 16:16:23 +0000

    conteudos e hands on aula 38

---
+++ CursoJavaBasico/aula38/Aluno.java
@@ -0,0 +1,43 @@
+package CursoJavaBasico.aula38;
+
+public class Aluno extends Pessoa {
+
+    private String curso;
+    private double[] notas;
+
+
+    public Aluno() {
+        super();
+    }
+
+    public Aluno(String nome, String endereco, String telefone, String curso){
+        super(nome, endereco, telefone);
+        this.curso = curso;
+    }
+
+    public String getCurso() {
+        return this.curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public double[] getNotas() {
+        return this.notas;
+    }
+
+    public void setNotas(double[] notas) {
+        this.notas = notas;
+    }
+
+    public double calcularMedia(){
+        return 0;
+    }
+
+    public boolean verificarAprovado(){
+        return true;
+    }
+
+
+}
---
+++ CursoJavaBasico/aula38/Teste.java
@@ -0,0 +1,5 @@
+package CursoJavaBasico.aula38;
+
+public class Teste {
+    
+}
