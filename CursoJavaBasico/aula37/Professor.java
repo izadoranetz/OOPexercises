81ba0aa65c97451ea80646d01e5e758214292da2
Izadora Netz <izadora.ns@gmail.com>
2023-04-17 16:06:01 +0000

    aula 37 - intro herança

---
+++ CursoJavaBasico/aula37/Professor.java
@@ -0,0 +1,30 @@
+package CursoJavaBasico.aula37;
+
+public class Professor extends Pessoa {
+
+    private double salario;
+    private String nomeCurso;
+
+
+    public double getSalario() {
+        return this.salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getNomeCurso() {
+        return this.nomeCurso;
+    }
+
+    public void setNomeCurso(String nomeCurso) {
+        this.nomeCurso = nomeCurso;
+    }
+
+    public double calcularSalarioLiquido() {
+        return 0;
+    }
+
+    
+}
---
+++ CursoJavaBasico/aula37/Teste.java
@@ -0,0 +1,21 @@
+package CursoJavaBasico.aula37;
+
+public class Teste {
+    public static void main(String[] args) {
+        // Aluno aluno = new Aluno();
+
+        // aluno.setNome("Maria");
+
+        // Professor professor = new Professor();
+
+        // professor.setNome("Joana");
+
+        // Pessoa é uma superclasse da classe aluno
+
+        // quando instancia de a partir da super classe, os atributos a que tem acesso são somente os da super classe,
+        // não tendo acesso aos atributos da classe específica, no exemplo, a classe Aluno
+        // Pessoa aluno = new Aluno();
+        // aluno.setNome("José");
+
+    }
+}
